945dac9a299a2cacfb9c3646ee7e68424bddfd08
李福东 <E_wind2017@163.com>
2017-10-13 13:22:38 +0800

    The second commit

---
+++ src/main/java/com/trs/other/pojo/My.java
@@ -0,0 +1,13 @@
+package com.trs.other.pojo;
+
+/**
+ * Title:forDev
+ * Description:
+ * Copyright: 2017 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
+ * Company:北京拓尔思信息技术股份有限公司(TRS)
+ * Project: HaierFramework
+ * Author: E_wind
+ * Create Time:2017/10/13 13:21
+ */
+public class My {
+}
